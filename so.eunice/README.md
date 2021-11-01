c629b01a379f34051937bf662caecf7513a214c6
Eunice So <euniceso.design@gmail.com>
2021-10-31 21:28:21 -0700

    m8

--- so.eunice/README.md
+++ so.eunice/README.md
@@ -4,3 +4,7 @@
 - http://euniceso.net
 - http://euniceso.net/aau/wnm608/so.eunice/
 - http://euniceso.net/aau/wnm608/so.eunice/styleguide
+
+## m8
+- http://euniceso.net/aau/wnm608/so.eunice/m8/notes/reading_data.php
+- http://euniceso.net/aau/wnm608/so.eunice/m8/admin/users.php
---
+++ so.eunice/m8/about.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>About</title>
+	<?php include "parts/meta.php"; ?>
+</head>
+<body>
+	<?php include "parts/navbar.php"; ?>
+
+	<div class="container">
+		<div class="card soft">
+			<h2>About</h2>
+			<p>Lorem ipsum, dolor sit, amet consectetur adipisicing elit. Exercitationem eligendi, dolorem asperiores aspernatur, consequatur sed consequuntur facere nulla. Iusto delectus libero perferendis. Soluta, consectetur iste fugiat excepturi explicabo deserunt quos?</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing, elit. Porro sed perferendis alias soluta quod quaerat ut, commodi id eum ex. Facilis fugit consectetur quibusdam consequatur, culpa laboriosam praesentium quis maiores!</p>
+		</div>
+
+	</div>
+</body>
+</html>
