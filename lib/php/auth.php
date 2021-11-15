22ac29891a57c536dd3f763b70f34353a82247ea
yoomiyi <90541259+yoomiyi@users.noreply.github.com>
2021-11-14 22:58:51 -0800

    Add files via upload

---
+++ lib/php/auth.php
@@ -0,0 +1,12 @@
+<?php
+
+function MYSQLIAuth() {
+	return [
+		 "localhost", // mysql host
+		 "yoomi_wnm608", //mysql username
+		 "Yoomiyi_54", //mysql password
+		 "yoomi_wnm608" //mysql database name
+
+	];
+
+}
