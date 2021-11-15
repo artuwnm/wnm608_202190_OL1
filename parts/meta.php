7dd3ec3f77ceafdcebf7f8b4aea62f9d376b19ce
yoomiyi <90541259+yoomiyi@users.noreply.github.com>
2021-11-14 22:59:11 -0800

    Add files via upload

---
+++ parts/meta.php
@@ -0,0 +1,10 @@
+
+<meta name="viewport" content="width=device-width">
+
+<base href="http://yoomiyi.com/aau/wnm608_yoomi/wnm608_202190_OL1/yoomi.yi/">
+
+<link rel="stylesheet" href="lib/css/styleguide.css">
+<link rel="stylesheet" href="lib/css/gridsystem.css">
+<link rel="stylesheet" href="css/storetheme.css">
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
---
+++ parts/navbar.php
@@ -0,0 +1,20 @@
+	
+
+
+
+	<header class="navbar">
+			<div class="container display-flex">
+				<div class="flex-none">
+					<h1>LUXLIFE</h1>
+				</div>
+				<div class="flex-stretch"></div>
+				<nav class="nav nav-flex flex-none">
+					<ul>
+						<li><a href="index.php">Home</a></li>
+						<li><a href="product_list.php">Store</a></li>
+						<li><a href="admin/about.php">About</a></li>
+						<li><a href="admin/product_cart.php">Cart</a></li>
+					</ul>
+				</nav>
+			</div>
+	</header>
