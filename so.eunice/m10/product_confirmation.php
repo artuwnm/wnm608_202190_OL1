1fbd7051c875299929b9f1ced4c2309cad53cd8d
Eunice So <euniceso.design@gmail.com>
2021-11-21 17:33:29 -0800

    m10

---
+++ so.eunice/m10/product_confirmation.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Cart</title>
+	<?php include "parts/meta.php"; ?>
+</head>
+<body>
+	<?php include "parts/navbar.php"; ?>
+		<div class="container">
+		<div class="card soft">
+			<h3>Thank you for your purchase</h3>
+			<p><a href="../so.eunice/m10/product_list.php">Continue Shopping</a></p>
+		</div>
+	</div>
+</body>
+</html>
