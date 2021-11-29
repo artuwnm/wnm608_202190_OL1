db0acca9f90d72b4b0da1340b9ad2fcaf3d5424a
yoomiyi <90541259+yoomiyi@users.noreply.github.com>
2021-11-28 18:18:21 -0800

    Add files via upload

--- parts/navbar.php
+++ parts/navbar.php
@@ -1,19 +1,27 @@
-	
+<?php
+
+include_once "lib/php/functions.php";
+
+?>
 
 
 
 	<header class="navbar">
 			<div class="container display-flex">
 				<div class="flex-none">
-					<h1>LUXLIFE</h1>
+					<h1>My Happy Soap</h1>
 				</div>
 				<div class="flex-stretch"></div>
 				<nav class="nav nav-flex flex-none">
 					<ul>
 						<li><a href="index.php">Home</a></li>
 						<li><a href="product_list.php">Store</a></li>
-						<li><a href="admin/about.php">About</a></li>
-						<li><a href="admin/product_cart.php">Cart</a></li>
+						<li><a href="about.php">About</a></li>
+						<li><a href="product_cart.php">
+							<span>Cart</span>
+							<span class="badge"><?=makeCartBadge(); ?></span>
+
+						</a></li>
 					</ul>
 				</nav>
 			</div>
